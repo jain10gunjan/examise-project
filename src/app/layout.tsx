0858909acd4c3259c06a6e8f6bc3c0b11b3ba7f7
Gunjan Jain <jain10gunjan@gmail.com>
2024-05-30 21:53:47 +0530

    Added Title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,7 @@ import Footer from "@/components/ui/Footer";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "",
+  title: "Examise",
   description: "Your One Stop For All MCQs Needs.",
 };
 
