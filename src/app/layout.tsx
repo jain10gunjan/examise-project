590631abb7092d7e14b4e2686930b472ac7b817e
Gunjan Jain <jain10gunjan@gmail.com>
2024-06-01 17:43:00 +0530

    added gtag.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/ui/Footer";
+import { GoogleAnalytics } from '@next/third-parties/google'
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,9 +20,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Navbar/>
+        <Navbar />
         {children}</body>
-        <Footer/>
+      <GoogleAnalytics gaId="G-L1XH39V6Z6" />
+      <Footer />
     </html>
   );
 }
