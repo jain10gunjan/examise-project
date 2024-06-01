c1e26b1d8199c4752f361e0de840eaaa182206e3
Gunjan Jain <jain10gunjan@gmail.com>
2024-06-01 14:16:58 +0530

    Added Sitemap and Build Folder.

---
+++ src/app/sitemaps/6/sitemap.tsx
@@ -0,0 +1,32 @@
+import { MetadataRoute } from 'next'
+
+export default async function sitemap(): Promise<MetadataRoute.Sitemap[]> {
+    const start = 25001;
+    const end = 30000;
+    interface ApiResponse {
+        data: {
+            data: []; // Adjusted to match the actual structure
+        };
+        total: number;
+    }
+
+    try {
+        const res = await fetch(`https://us-east-1.aws.data.mongodb-api.com/app/aptitude_tracker_api-fjroz/endpoint/sitemap?start=${start}&end=${end}`);
+        if (!res.ok) {
+            throw new Error('Network response was not ok');
+        }
+        const data: ApiResponse | any = await res.json();
+
+        var result =  data.data.map((id: string) => ({
+            url: `https://app.examise.in${id}`,
+            lastModified: new Date(),
+            changeFrequency: 'yearly',
+            priority: 0.9,
+        }));
+
+    } catch (error: any) {
+        console.error(error.message);
+    }
+
+    return result;
+}
