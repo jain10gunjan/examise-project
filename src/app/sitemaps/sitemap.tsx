12124938f2727209c6b9ed2923b837302edaed50
Gunjan Jain <jain10gunjan@gmail.com>
2024-07-06 09:36:04 +0530

    Sitemap Update.

--- src/app/sitemaps/sitemap.tsx
+++ src/app/sitemaps/sitemap.tsx
@@ -3,19 +3,25 @@ import { MetadataRoute } from 'next'
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: 'https://examise.in/calculators/age-calculator/',
+      url: 'https://www.examise.in/calculators/protein-calculator/',
       lastModified: new Date(),
       changeFrequency: 'yearly',
       priority: 0.9,
     },
     {
-      url: 'https://examise.in/calculators/bmi-calculator/',
+      url: 'https://www.examise.in/calculators/bmi-calculator/',
       lastModified: new Date(),
       changeFrequency: 'yearly',
       priority: 0.9,
     },
     {
-      url: 'https://examise.in/calculators/macro-calculator/',
+      url: 'https://www.examise.in/calculators/macro-calculator/',
+      lastModified: new Date(),
+      changeFrequency: 'yearly',
+      priority: 0.9,
+    },
+    {
+      url: 'https://www.examise.in/calculators/mathematics/2d-distance-calculator',
       lastModified: new Date(),
       changeFrequency: 'yearly',
       priority: 0.9,
